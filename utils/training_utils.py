60bd11395ef67c03b200590c9cf339aa6c6a14b8
Hamidreza Sadeghi <sadeghi.hamidreza1400@gmail.com>
2022-09-05 19:57:31 +0430

    Update training_utils.py

--- utils/training_utils.py
+++ utils/training_utils.py
@@ -93,7 +93,8 @@ def train_step(model,
     comma_loss = loss_function(input, 
                          comma_tags,
                          comma_tag_scores,
-                         loss_object = loss_object)
+                         loss_object = loss_object,
+                         weights_for_labels = [0.15, 0.85])
 
 
     total_loss = kasreh_loss + comma_loss
