25599edac6f7d0adf484ec2a192cae2e3c480aa2
Hamidreza Sadeghi <sadeghi.hamidreza1400@gmail.com>
2022-09-07 10:37:43 +0430

    Update download_models.py

--- Joint_kasreh_comma/download_models.py
+++ Joint_kasreh_comma/download_models.py
@@ -48,8 +48,8 @@ def downloader():
         {"url" : "https://drive.google.com/file/d/1-RzQtWWGdix7h9y_jLk5xmVItTx6p6nj/view?usp=sharing",
         "output" : join(bert_ckpt_dir, "config.json")},
 
-        {"url" : "https://drive.google.com/file/d/1-PuRT0euYHwkhJI5S4_XeRpBQ73ximW0/view?usp=sharing",
-        "output" : join(our_model_ckpt_dir, "best_global_time_2_val_accuracy=0.9825.pt")}
+        {"url" : "https://drive.google.com/file/d/1-NikeDA8_ZkSThPANX9Y5tattCKd-xWt/view?usp=sharing",
+        "output" : join(our_model_ckpt_dir, "best_global_time_2_val_accuracy=0.9718.pt")}
     ]
 
     for obj in files:
