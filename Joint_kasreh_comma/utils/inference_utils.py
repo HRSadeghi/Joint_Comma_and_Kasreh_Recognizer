f74e59fa27907c9db99ca8d357d3d71c0be7f484
Hamidreza Sadeghi <sadeghi.hamidreza1400@gmail.com>
2022-09-07 11:47:56 +0430

    Update inference_utils.py

--- Joint_kasreh_comma/utils/inference_utils.py
+++ Joint_kasreh_comma/utils/inference_utils.py
@@ -56,7 +56,8 @@ def return_sen_to_real_form(tokenizer, input_sen, kasreh_tags, comma_tags):
                 out += ' '
 
         
-
+    out = out.replace('، ،', '،').replace('،،', '،')
+    out = ' '.join(out.split())
     return out.replace('، ،', '،').replace('،،', '،')
 
 
